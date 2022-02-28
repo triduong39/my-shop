7cfebc53b708391d446f7355312fefc2b112ab5e
triduong39 <tri.duong@hdwebsoft.io>
2022-02-28 16:44:23 +0700

    feature/Product: refetch pagination when Params state change

--- src/features/product/routes/Products.tsx
+++ src/features/product/routes/Products.tsx
@@ -1,33 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { Typography } from '@mui/material';
 import { useAppDispatch } from '../../../hooks/useAppDispatch';
 import { useAppSelector } from '../../../hooks/useAppSelector';
 import { fetchListProduct } from '../redux/productSlice';
 import { useSearchParams } from 'react-router-dom';
-import { Pagination } from '../types';
 
 export default function Products() {
     const dispatch = useAppDispatch();
     const [searchParams, setSearchParams] = useSearchParams();
-
-    console.log('searchParams', searchParams);
-    console.log('get', searchParams.get('_page'));
-
-    const pagination: Pagination = {
-        _page: Number(searchParams.get('_page')) || 1,
-        _limit: Number(searchParams.get('_limit')) || 10,
-    };
-
     const listProduct = useAppSelector((state) => state.product.listProduct);
+
+    const _page = searchParams.get('_page');
+    const _limit = searchParams.get('_limit');
+
+    const pagination = useMemo(() => {
+        return {
+            _page: Number(_page) || 1,
+            _limit: Number(_limit) || 10,
+        };
+    }, [_page, _limit]);
 
     console.log('listProduct', listProduct);
 
     useEffect(() => {
         dispatch(fetchListProduct(pagination));
-    }, []);
+    }, [pagination]);
+
     return (
-        <Typography variant="h3" component="h1" align="center" sx={{ mb: 3 }}>
-            Products bibam
-        </Typography>
+        <>
+            <Typography variant="h3" component="h1" align="center" sx={{ mb: 3 }}>
+                Products bibam
+            </Typography>
+            <button
+                onClick={() => {
+                    setSearchParams({ _page: '1' });
+                }}
+            >
+                click here
+            </button>
+        </>
     );
 }
